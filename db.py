2a6c84adf9c088d5fc7091ef6bbf4211a1c08186
DESKTOP-4TV85P3\ZA bawani <ahsanarsalan41@gmail.com>
2025-01-20 12:55:54 +0100

    Add database configuration and setup with SQLAlchemy for PostgreSQL integration

---
+++ db.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Define the PostgreSQL connection string
+DATABASE_URL = "postgresql://postgres:12345@localhost:5432/bookstore"
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a Base class for our models to inherit from
+Base = declarative_base()
+
+# Dependency to get the session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_table():
+    Base.metadata.create_all(bind=engine)
